81d49fbc7ae8dd6051391bc019911e7fbb5169e2
peter.georgiev <pesho10@yahoo.com>
2015-01-02 16:58:09 +0100

    introduce database connection

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,13 +1,43 @@
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @ManagedBean
 @ApplicationScoped
 public class HelloWorld {
+    private DataSource dsc;
+
     public HelloWorld() {
-        System.out.println("WelcomeBean instantiated");
+        try {
+            Context initCtx = new InitialContext();
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
+            dsc = (DataSource)
+                    envCtx.lookup("jdbc/DSTest");
+        } catch (NamingException ex) {
+            System.out.println("WelcomeBean instantiated");
+        }
     }
     public String getMessage() {
-        return "I'm alive!";
+        String data = "";
+        try {
+            Connection conn = this.dsc.getConnection();
+            PreparedStatement s = conn.prepareStatement("SELECT * FROM users");
+            s.execute();
+            ResultSet set = s.getResultSet();
+            while (set.next()) {
+                data += set.getInt("id") + ":" + set.getString("userscol");
+                data += "<br/>";
+            }
+        } catch (SQLException ex) {
+            System.err.println("Catched");
+        }
+        return data;
     }
 }
